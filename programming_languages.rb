0b2fd8780ff442129e3e9b30ba2c4080100dff56
cd <coraldarling@outlook.com>
2018-10-02 17:45:31 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, language_hash|
+    language_hash.each do |language, attribute_hash|
+      attribute_hash.each do |type, value|
+        if new_hash[language]
+          new_hash[language][:style] << style
+        else
+          new_hash[language] = {type => value, :style => [style]}
+        end
+      end
+    end
+  end
+  new_hash
 end
