eeeef5d87a45558ef6f57de1841a2f2697915717
github username <github email address>
2018-10-05 02:31:50 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  reformatted = {}
+
+  languages.each do |language_style, language_list|
+    language_list.each do |language_name, language_type|
+      if reformatted[language_name]
+        reformatted[language_name][:type] = language_type[:type]
+      else
+        reformatted[language_name] = { type: language_type[:type]  }
+      end
+
+      if reformatted[language_name][:style]
+        reformatted[language_name][:style] << language_style
+      else
+        reformatted[language_name][:style] = []
+        reformatted[language_name][:style] << language_style
+      end
+    end
+  end
+
+  reformatted
 end
